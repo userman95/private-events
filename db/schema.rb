b3e3d55e4469952ff0e226392ef6211419ff0d77
userman95 <orestismail71@gmail.com>
2019-05-17 13:34:25 +0300

    adding diagram removing email column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_205153) do
+ActiveRecord::Schema.define(version: 2019_05_17_101614) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "user_id"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2019_05_14_205153) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
